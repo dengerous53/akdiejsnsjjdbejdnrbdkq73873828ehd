b84e34d510cb29f395be9764941b421da27159fd
Badsha323 <98164078+Badsha323@users.noreply.github.com>
2022-07-24 18:30:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -50,6 +50,22 @@ async def start(client, message):
             InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
             InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
+        reply1 = await query.message.reply_text(
+            text="â–¢â–¢â–¢"
+        )
+        await asyncio.sleep(0.5)
+        reply2 = await reply1.edit_text(
+            text="â–£â–¢â–¢"
+        )
+        await asyncio.sleep(0.5)
+        reply3 = await reply2.edit_text(
+            text="â–£â–£â–¢"
+        )
+        await asyncio.sleep(0.5)
+        reply4 = await reply3.edit_text(
+            text="â–£â–£â–£"
+        )
+        await reply4.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
@@ -96,6 +112,22 @@ async def start(client, message):
             InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
             InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
+        reply1 = await query.message.reply_text(
+            text="â–¢â–¢â–¢"
+        )
+        await asyncio.sleep(0.5)
+        reply2 = await reply1.edit_text(
+            text="â–£â–¢â–¢"
+        )
+        await asyncio.sleep(0.5)
+        reply3 = await reply2.edit_text(
+            text="â–£â–£â–¢"
+        )
+        await asyncio.sleep(0.5)
+        reply4 = await reply3.edit_text(
+            text="â–£â–£â–£"
+        )
+        await reply4.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
